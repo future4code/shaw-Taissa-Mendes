46251dfc6d08024963237cbc32c9f255fce8b21b
TaissaRGSMendes <98991691+TaissaRGSMendes@users.noreply.github.com>
2022-02-20 13:03:27 -0300

    Update exercicios.js

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -79,37 +79,61 @@ function calculaIngressosEspetaculo(custo, valorIngresso) {
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
-  // implemente sua lógica aqui
+  const condicao = "a b c" === "a b c d"
+  console.log(condicao)
 
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
-  // implemente sua lógica aqui
+  const racasDeCachorros = ['Poodle', 'Caramelo', 'Bulldog', 'Pitbul', 'Lhasa']
+  const numero = prompt("Didite um numero de 0 a 5")
+  console.log(numero) /*Número escolhido foi zero */
+  const racaEscolhida = racasDeCachorros[0]
+  console.log(racaEscolhida) /*O resultado foi Poodle */
 
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
-  // implemente sua lógica aqui
+  const numeros = [1, 2, 3, 4, 5]
+  const ultimoNumero = numeros[4]
+  console.log(ultimoNumero) /*Resposta do console = 5 */
 
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
-  // implemente sua lógica aqui
+  let numeros = array [0]
+  let qElem = array.length()
+  let ultimoElem = array [-1]
+  array [0] = ultimoElem
+  array [qElem -1] = numeros
+  return array
 
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
-  // implemente sua lógica aqui
+  return string1.toUpperCase() === string2.toUpperCase()
 
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+  const anoAtual = prompt("Digite o ano atual") /*2020 */
+  const anoDeNascimento = prompt("Digite seu ano de nascimento") /* 2000 */
+  const anoDeEmissaoDoRG = prompt("Digite o ano de emissão do seu RG") /*2015 */
+  const idade = anoAtual-anoDenascimento
+  console.log(idade) /*Resultado = a 20 no console */
+  const anoDoRG = anoAtual-anoDeEmissaoDoRG
+  console.log(anoDoRG) /* Resultado = a 5 no console */
+  const renova = (idade <= 20 && anoDoRG >=5) || ((idade > 20 && idade <= 50) && anoDoRG >= 10) || (idade > 50 && anoDoRG >= 15)
+  console.log(renova) /* Resultado = a true no console */
+
+  /* GOSTARIA DE DIVIDIR COM VOCÊS QUE A QUESTÃO 13 FOI A ÚNICA QUESTÃO QUE O CÓDIGO NÃO FOI FEITO POR MIM E SIM
+  POR UMA COLEGA DE TURMA. EU NÃO SOUBE FAZER A QUESTÃO E ACHEI O CÓDIGO DELA O MÁXIMO, SEM FALAR QUE VAI SERVIR PARA EU ESTUDAR
+  E APRENDER A USAR OS COMPARADORES  */
 
 }
 
@@ -121,6 +145,12 @@ function checaAnoBissexto(ano) {
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
+  const idade = prompt("Você tem 18 anos?") /* Não */
+  const escolaridade = prompt("Voce concluiu o ensino médio?") /*Sim */
+  const disponibilidade = prompt("Você possui disponibilidade total de horário?") /* Sim */
+  const idade = false
+  const escolaridade = true
+  const disponibilidade = true
+  console.log(idade && escolaridade && disponibilidade) /* Resposta do console = a false */
 
 }
